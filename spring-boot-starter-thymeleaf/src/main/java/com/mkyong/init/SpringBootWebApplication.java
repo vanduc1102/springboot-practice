54de46019e7a98f35608d99c98960de265300a7f
Duc Nguyen <vanducld@gmail.com>
2018-07-22 13:09:12 +0700

    Add gs rest-service

--- spring-boot-starter-thymeleaf/src/main/java/com/mkyong/init/SpringBootWebApplication.java
+++ spring-boot-starter-thymeleaf/src/main/java/com/mkyong/init/SpringBootWebApplication.java
@@ -1,4 +1,4 @@
-package com.mkyong;
+package com.mkyong.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootWebApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootWebApplication.class, args);
-	}
-
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(SpringBootWebApplication.class, args);
+  }
 }
